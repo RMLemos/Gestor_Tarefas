60bd56268a856d94e78f8900698d4c71bfcebca1
RMLemos <143395332+RMLemos@users.noreply.github.com>
2023-09-23 12:32:13 +0100

    ci_06

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -3,11 +3,25 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\TasksModel;
+use App\Models\UsuariosModel;
 
 class Main extends BaseController
 {
     public function index()
     {
-        echo "Hello World!";
+        // echo "Hello World!";
+        $model_usuarios = new UsuariosModel();
+        $usuarios = $model_usuarios->findAll();
+
+        // dd($usuarios);
+        echo 'pre';
+        print_r($usuarios);
+
+        $model_tasks = new TasksModel();
+        $tasks = $model_tasks->findAll();
+
+        // echo 'pre';
+        // print_r($tasks);
     }
 }

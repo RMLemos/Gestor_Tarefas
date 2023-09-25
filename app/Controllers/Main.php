a8760e7db1902f6af018c43ddac009cb1d7276c3
RMLemos <143395332+RMLemos@users.noreply.github.com>
2023-09-25 10:15:05 +0100

    ci_08

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -10,16 +10,52 @@ class Main extends BaseController
 {
     public function index()
     {
-        //
+        //index
     }
     
     public function login()
     {
-        return view('login_frm');
+        $data = [];
+
+        //check for valit«dations erros
+        $validation_errors = session()->getFlashdata('validation_errors');
+        if($validation_errors){
+            $data['validation_errors'] = $validation_errors;
+        }
+
+        return view('login_frm', $data);
     }
 
     public function login_submit()
     {
-        echo 'login submit';
+
+            //form validation
+        $validation =$this->validate(
+             //validation rules
+            [
+                'text_usuario' => 'required',
+                'text_senha' => 'required',
+            ],
+            //validation errors
+            [
+                'text_usuario' => [
+                    'required' => 'O campo Nome Utilizador é obrigatório.'
+                ],
+                'text_senha' => [
+                    'required' => 'O campo Password é obrigatório.'
+                ]
+
+            ]
+        );
+            
+        if(!$validation){
+            return redirect()->to('login')->withInput()->with('validation_errors', $this->validator->getErrors());
+        }
+
+       $usuario =$this->request->getPost('text_usuario');
+       $senha =$this->request->getPost('text_senha');
+
+       dd([$usuario, $senha]);
+
     }
 }
--- app/Views/login_frm.php
+++ app/Views/login_frm.php
@@ -7,15 +7,17 @@
         <div class="row justify-content-center">
             <div class="col-md-4 col-sm-6 col-10">
                 <div class="card bg-secundary text-light rounded-3 p-5">
-                    <?= form_open('login_submit') ?>
+                    <?= form_open('login_submit', ['novalidate' => true]) ?>
 
                     <h3 class="text-center">Login</h3>
                     <hr>
                     <div class="mb-3">
-                        <input type="text" class="form-control" name="text_usuario" placeholder="Nome utilizador" required>
+                        <input type="text" class="form-control" name="text_usuario" placeholder="Nome utilizador" required value="<?= old('text_usuario', '') ?>">
+                        <?= !empty($validation_errors['text_usuario']) ? '<p class="text-danger">' . $validation_errors['text_usuario'] . '</p>' : '' ?>
                     </div>
                     <div class="mb-3">
-                        <input type="password" class="form-control" name="text_senha" placeholder="Password" required>  
+                        <input type="password" class="form-control" name="text_senha" placeholder="Password" required value="<?= old('text_senha', '') ?>"> 
+                        <?= !empty($validation_errors['text_senha']) ? '<p class="text-danger">' . $validation_errors['text_senha'] . '</p>' : '' ?> 
                     </div>
                     <div class="mb-3 text-end">
                         <button class="btn btn-light w-10">Entrar</button>  
